7fbf750149a53f73092b0d62e9224576c54e37a8
Aditya Saxena <aaddii091@gmail.com>
2024-04-14 22:37:30 +0530

    Cors

--- proxy-server.js
+++ proxy-server.js
@@ -18,7 +18,7 @@ app.use((req, res, next) => {
   );
   next();
 });
-
+app.use(cors);
 // Proxy endpoint
 app.get("/search", async (req, res) => {
   try {
@@ -32,36 +32,6 @@ app.get("/search", async (req, res) => {
   }
 });
 // Proxy endpoint for uploading image
-app.post("/resume", async (req, res) => {
-  try {
-    // Extract image data from the request body
-    const { blob } = req.body;
-
-    // Create FormData object and append image data
-    const formData = new FormData();
-    formData.append("image", blob, "image.png");
-
-    // Set headers for the request
-    const config = {
-      headers: {
-        "Content-Type": "multipart/form-data",
-      },
-    };
-
-    // Make the POST request to the target endpoint
-    const response = await axios.post(
-      "https://us-central1-register-555aa.cloudfunctions.net/summaryAPI/upload-image",
-      formData,
-      config
-    );
-
-    // Send the response from the target endpoint back to the client
-    res.send(response.data);
-  } catch (error) {
-    console.error("Error proxying request:", error);
-    res.status(500).send(error);
-  }
-});
 app.listen(PORT, () => {
   console.log(`Proxy server is running on port ${PORT}`);
 });
